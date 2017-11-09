eed2e7c3ab8da1a950c76dd59a19a288b622c74b
Alaa Attya <alaa.attya@tajawal.com>
2017-11-09 15:29:11 +0200

    PS-2017.2.1 <alaaattya@Alaas-MacBook-Pro.local Create baseRefactoring.xml, markdown.xml, PHP Interface Doc Comment.php, ide.general.xml, project.default.xml, diff.xml, DEV.xml, ea_extended.xml, git.xml, other.xml, Default.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, php.xml, code.style.schemes.xml, watcherDefaultTasks.xml, Default _3_.xml, path.macros.xml, databaseDrivers.xml, PHP Function Doc Comment.php, Default.xml, Default _5_.xml, Default _2_.xml, laf.xml, Default _1_.xml, Default _4_.xml, databaseSettings.xml, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/includes/PHP Function Doc Comment.php
@@ -0,0 +1,10 @@
+/**
+ * DESC
+${PARAM_DOC}
+#if (${TYPE_HINT} != "void") * @return ${TYPE_HINT}
+#end
+${THROWS_DOC}
+ *
+ * @author Alaa Attya <alaa.attya@tajawal.com>
+ *
+ */
---
+++ fileTemplates/includes/PHP Interface Doc Comment.php
@@ -0,0 +1,7 @@
+/**
+ * Interface ${NAME}
+#if (${NAMESPACE}) * @package ${NAMESPACE}
+#end
+ * @author Alaa Attya <alaa.attya@tajawal.com>
+ *
+ */
